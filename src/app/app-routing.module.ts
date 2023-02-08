758586a3e2cda6916055276879d59832a4009d48
googray <shat.rom07@gmail.com>
2023-02-08 23:08:19 +0100

    implement lazyloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CounterComponent } from './counter/counter/counter.component';
 import { HomeComponent } from './home/home.component';
-import { AddPostComponent } from './posts/add-post/add-post.component';
-import { EditPostComponent } from './posts/edit-post/edit-post.component';
-import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 const routes: Routes = [
   {
@@ -13,15 +9,12 @@ const routes: Routes = [
   },
   {
     path: 'counter',
-    component: CounterComponent,
+    loadChildren: () =>
+      import('./counter/counter.module').then((m) => m.CounterModule),
   },
   {
     path: 'posts',
-    component: PostsListComponent,
-    children: [
-      { path: 'add', component: AddPostComponent },
-      { path: 'edit/:id', component: EditPostComponent },
-    ],
+    loadChildren: () => import('./posts/post.module').then((m) => m.PostModule),
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,15 @@ import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter/counter.component';
-import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import { StoreModule } from '@ngrx/store';
-import { RouterModule } from '@angular/router';
-import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
-import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { PostsListComponent } from './posts/posts-list/posts-list.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { appReducer } from './store/app.store';
-import { AddPostComponent } from './posts/add-post/add-post.component';
-import { EditPostComponent } from './posts/edit-post/edit-post.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent,
-    CounterOutputComponent,
-    CounterButtonsComponent,
-    CustomCounterInputComponent,
-    HomeComponent,
-    HeaderComponent,
-    PostsListComponent,
-    AddPostComponent,
-    EditPostComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -41,6 +22,5 @@ import { EditPostComponent } from './posts/edit-post/edit-post.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  // exports: [RouterModule],
 })
 export class AppModule {}
